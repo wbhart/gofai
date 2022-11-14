79dcc00883c22a6bc06bbf51899c13b0923d088b
William Hart <goodwillhart@googlemail.com>
2022-11-14 17:42:15 +0000

    Better boxes.

--- interface/console.py
+++ interface/console.py
@@ -9,15 +9,21 @@ def init_console():
 
     width = curses.COLS
     height = curses.LINES
-    win1_height = (height - 1)//2
-    win2_height = (height - 1)//2
+    win1_height = height//2
+    win2_height = height//2
 
     # divide the screen into two windows less than half the height
     win1 = curses.newwin(win1_height, width, 0, 0)
-    win2 = curses.newwin(win2_height, width, win1_height, 0)
+    win2 = curses.newwin(win2_height, width, win1_height - 1, 0)
     
-    win1.box()
-    win2.box()
+    win1.border(curses.ACS_VLINE, curses.ACS_VLINE,
+                curses.ACS_HLINE, curses.ACS_HLINE,
+                curses.ACS_ULCORNER, curses.ACS_URCORNER,
+                curses.ACS_LTEE, curses.ACS_RTEE)
+    win2.border(curses.ACS_VLINE, curses.ACS_VLINE,
+                curses.ACS_HLINE, curses.ACS_HLINE,
+                curses.ACS_LTEE, curses.ACS_RTEE,
+                curses.ACS_LLCORNER, curses.ACS_LRCORNER)
 
     win1.move(1, 1)
     win2.move(1, 1)
