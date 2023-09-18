157e2d3cd617beb40f446d46d0dcb61c67d9d769
Valentin Imbach <valentin.imbach@gmail.com>
2023-09-18 15:30:07 +0200

    web-interface files

---
+++ web/script.js
@@ -0,0 +1,18 @@
+
+// display the given string and wait for acknowledgement from the user
+eel.expose(dialog);
+function dialog(str) {
+	alert(str);
+}
+
+// display the given string in the status area
+eel.expose(status);
+function status(str) {
+	// TODO
+}
+
+// populate list of displayed library results
+eel.expose(populate_library)
+function populate_library(list) {
+	// TODO
+}
