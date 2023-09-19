c890e745504689e676c6ff5c3b2cba4e2c391610
Valentin Imbach <valentin.imbach@gmail.com>
2023-09-19 12:36:08 +0200

    js functionality, css cleanup, web-interface documentation

---
+++ web/documentation.md
@@ -0,0 +1,64 @@
+
+## JavaScript Functions Exposed to Python:
+
+`status(str) -> none`  
+set the displayed status message to the string
+
+`info(str) -> none`  
+display the given string and wait for user acknowledgement
+
+`dialog(str) -> str/none`  
+display the given string and wait for user string input, returns input unless cancelled
+
+`populate_library([str]) -> none`  
+set the list of displayed lines in the library results
+
+`populate_quantifiers([str]) -> none`  
+set the list of displayed lines in the quantifier zone
+
+`populate_hypothesis([str]) -> none`  
+set the list of displayed lines in the hypothesis zone
+
+`populate_targets([str]) -> none`  
+set the list of displayed lines in the target zone
+
+## Expected Python Functions Exposed to JavaScript
+
+`move_clicked(int) -> none`  
+called when a move is clicked, passing the move type  
+(0 = Modus Ponens, 1 = Modus Tollens, 2 = Skolomize, 3 = Automation)
+
+`qunatifier_clicked(int) -> none`  
+called when a line in the qunatifier zone is clicked, passing the line number (0-indexed)
+
+`hypothesis_clicked(int) -> none`  
+called when a line in the hypothesis zone is clicked, passing the line number (0-indexed)
+
+`target_clicked(int) -> none`  
+called when a line in the target zone is clicked, passing the line number (0-indexed)
+
+`library_search(str) -> none`
+called whenever the string in the library search field changes, passing the current string
+
+`new_proof() -> none`  
+called when the "new proof" button is clicked
+
+`restart_proof() -> none`  
+called when the "restart proof" button is clicked
+
+`export_proof() -> none`  
+called when the "export proof" button is clicked
+
+## ToDo-List
+
+### Protocol Improvements
+- pass library serach results as python dicts, with fields for title, preview, tags, etc
+- pass mathematical terms as trees (python dicts) to allow selection of subexpressions
+- pass information as to what is currently clickable/selectable
+- use unique identifiers instead of line numbers (to allow reordering/sorting)
+
+### Design Improvements
+- highlighting when selecting lines during a move
+- greying out things not currently clickable/selectable
+- allow reordering/sorting of lines
+- add tutorial/instructions
--- web/script.js
+++ web/script.js
@@ -1,18 +1,153 @@
 
+// --- Functions exposed to python ---
+
 // display the given string and wait for acknowledgement from the user
+eel.expose(info);
+function info(str) {
+	alert(str);
+}
+
+// display the given string and wait for user string input
 eel.expose(dialog);
 function dialog(str) {
-	alert(str);
+	return prompt(str);
 }
 
 // display the given string in the status area
 eel.expose(status);
 function status(str) {
-	// TODO
+	document.getElementById("status").innerHTML = str;
 }
 
 // populate list of displayed library results
 eel.expose(populate_library)
 function populate_library(list) {
+	var div = document.getElementById("lib-box").getElementsByClassName("box-body")[0];
+	div.innerHTML = "";
+
+	for (let i = 0; i < list.length; i++) {
+		var line = document.createElement('div');
+		line.classList.add("line", "entry")
+		line.onclick = function() {library_clicked(i)};
+		line.innerHTML = list[i];
+
+		div.appendChild(line);
+	}
+	
+}
+
+// populate quantifier zone
+eel.expose(populate_quantifiers)
+function populate_quantifiers(list) {
+	var div = document.getElementById("quant-box").getElementsByClassName("box-body")[0];
+	div.innerHTML = "";
+
+	for (let i = 0; i < list.length; i++) {
+		var line = document.createElement('div');
+		line.classList.add("line", "quant");
+		line.onclick = function() {quantifier_clicked(i)};
+
+		var name = document.createElement('span');
+		name.classList.add("zone-line-name");
+		name.innerHTML = "\\(\\bf Q_" + i + "\\)";
+
+		line.appendChild(name);
+		line.innerHTML += list[i];
+		div.appendChild(line);
+	}
+	MathJax.typeset();
+}
+
+// populate hypothesis zone
+eel.expose(populate_hypothesis)
+function populate_hypothesis(list) {
+	var div = document.getElementById("hyp-box").getElementsByClassName("box-body")[0];
+	div.innerHTML = "";
+
+	for (let i = 0; i < list.length; i++) {
+		var line = document.createElement('div');
+		line.classList.add("line", "hyp");
+		line.onclick = function() {hypothesis_clicked(i)};
+
+		var name = document.createElement('span');
+		name.classList.add("zone-line-name");
+		name.innerHTML = "\\(\\bf H_" + (i+1) + "\\)";
+
+		line.appendChild(name);
+		line.innerHTML += list[i];
+		div.appendChild(line);
+	}
+	MathJax.typeset();
+}
+
+// populate target zone
+eel.expose(populate_targets)
+function populate_targets(list) {
+	var div = document.getElementById("targ-box").getElementsByClassName("box-body")[0];
+	div.innerHTML = "";
+
+	for (let i = 0; i < list.length; i++) {
+		var line = document.createElement('div');
+		line.classList.add("line", "target");
+		line.onclick = function() {target_clicked(i)};
+
+		var name = document.createElement('span');
+		name.classList.add("zone-line-name");
+		name.innerHTML = "\\(\\bf T_" + (i+1) + "\\)";
+
+		line.appendChild(name);
+		line.innerHTML += list[i];
+		
+		div.appendChild(line);
+	}
+	MathJax.typeset();
+}
+
+// --- Functions forwarding to python ---
+
+function quantifier_clicked(n) {
+	console.log("quantifier " + n +  " clicked!");
 	// TODO
 }
+
+function hypothesis_clicked(n) {
+	console.log("hypothesis " + n +  " clicked!");
+	// TODO
+}
+
+function target_clicked(n) {
+	console.log("target " + n +  " clicked!");
+	// TODO
+}
+
+function move_clicked(n) {
+	console.log("move " + n +  " clicked!");
+	// TODO
+}
+
+function library_clicked(n) {
+	console.log("library line " + n +  " clicked!");
+	// TODO
+}
+
+function new_proof() {
+	console.log("new proof!");
+	// TODO
+}
+
+function restart_proof() {
+	console.log("restart proof!");
+	// TODO
+}
+
+function export_proof() {
+	console.log("export proof!");
+	// TODO
+}
+
+function library_search(str) {
+	console.log("search library for: " + str)
+	// TODO
+}
+
+
